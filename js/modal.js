64ff760fcbdba44e4f1268ea761dbc2c030080ef
Andrey Moskalets <voxport@gmail.com>
2023-07-19 22:49:15 +0200

    temp

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,14 @@
-const hero__button = document.querySelector('.hero__button ')
+const hero__button = document.querySelector('.hero__button')
 const modalWindow = document.querySelector('.modal-window')
 const modalWindow__buttonClose = document.querySelector('.modal-window__button-close')
+const formEntry = document.querySelector('.form-entry')
 
 hero__button.addEventListener('click', () => {
-    modalWindow.style.display='block'
+    modalWindow.style.display = 'block';
 })
 
 modalWindow__buttonClose.addEventListener('click', () => {
-    modalWindow.style.display='none'
+    modalWindow.style.display = 'none';
 })
 
 document.addEventListener('click', (event) => {
@@ -18,3 +19,9 @@ document.addEventListener('click', (event) => {
         modalWindow.style.display = 'none';
     }
 })
+
+formEntry.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    modalWindow.style.display = 'none';
+});
